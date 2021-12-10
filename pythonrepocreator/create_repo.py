2d8a2f99a18054a2a995b99b642a8aba8e4fef67
RJGY <rjgy2yu@gmail.com>
2021-12-11 02:38:56 +1100

    added init commands

--- pythonrepocreator/create_repo.py
+++ pythonrepocreator/create_repo.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
+# TODO: make this work with bash
 import requests
-from pprint import pprint
 from decouple import config
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -15,17 +18,28 @@ repo_is_private = args.is_private
 API_URL = "https://api.github.com"
 
 if (repo_is_private):
-    api_data = '{ "name" : ' + repo_name + ', "private" : true }'
+    api_data = '{ "name" : "' + repo_name + '", "private" : true }'
 else: 
-    api_data = '{ "name" : ' + repo_name + ', "private" : false }'
+    api_data = '{ "name" : "' + repo_name + '", "private" : false }'
 
-headerdata = {
+header_data = {
     "Authorization" : "token " + config('TOKEN'),
     "Accept" : "applictation/vnd.github.v3+json"
 }
 try:
-    req = requests.post(API_URL + "/user/repos", data=apidata, headers=headerdata)
+    req = requests.post(API_URL + "/user/repos", data=api_data, headers=header_data)
     req.raise_for_status()
 
 except requests.exceptions.RequestException as err:
+    raise SystemExit(err)
+
+try: 
+    os.system("mkdir " + repo_name)
+    os.chdir(repo_name)
+    os.system("git init")
+    os.system("git remote add origin " + config('GITHUB') + repo_name + ".git")
+    os.system("echo # " + repo_name + " >> README.md")
+    os.system('git add . && git commit -m "Initial commit" && git push --set-upstream origin main')
+
+except FileExistsError as err:
     raise SystemExit(err)
